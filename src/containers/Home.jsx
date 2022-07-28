a19e06949e1736f0199b2bd658be69e5a4765776
unknown <akilapanunayan@gmail.com>
2022-07-28 19:53:04 +0100

    first commit

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         {/* <button className="rounded-lg shadow-lg tracking-widest bg-blue-900 text-white text-2xl uppercase"> */}
         <Link
           to="/test"
-          className="text-center rounded-lg shadow-lg tracking-widest bg-blue-800 text-white text-2xl uppercase py-3 w-1/4"
+          className="text-center rounded-lg shadow-lg tracking-widest bg-blue-800 text-white text-2xl uppercase py-3 w-2/3 md:1/2 lg:w-1/3"
         >
           Take Test
         </Link>
