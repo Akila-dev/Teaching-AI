4870c41b978542b2e8512d68d68a8706a808cad9
unknown <akilapanunayan@gmail.com>
2022-07-29 01:29:04 +0100

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { BsQuestionLg } from "react-icons/bs";
 
@@ -6,11 +7,15 @@ const Header = () => {
   return (
     <div className="py-5 fixed bg-white shadow-lg w-full rounded-b-2xl">
       <div className="container flex justify-between items-center">
-        <img src={logo} alt="logo" className="w-20 md:w-24" />
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-20 md:w-24" />
+        </Link>
         <div></div>
-        <div className="p-4 md:p-5 bg-blue-800 rounded-full">
-          <BsQuestionLg className="text-white" />
-        </div>
+        <Link to="/test">
+          <div className="p-4 md:p-5 bg-blue-800 rounded-full">
+            <BsQuestionLg className="text-white" />
+          </div>
+        </Link>
       </div>
     </div>
   );
