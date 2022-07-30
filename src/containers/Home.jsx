2201f3d2b76081071d428e74e1ae9ae2138b046e
unknown <akilapanunayan@gmail.com>
2022-07-30 06:03:28 +0100

    first commit

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -6,16 +6,17 @@ const Home = () => {
   return (
     <>
       <Navbar position={"fixed"} />
-      <div id="home" className="h-screen w-full container">
+      <div
+        id="home"
+        className="h-screen w-full container fixed bottom-0 left-0"
+      >
         <div className="flex w-full h-full justify-center lg:items-center items-end pb-14 lg:pb-0">
-          {/* <button className="rounded-lg shadow-lg tracking-widest bg-blue-900 text-white text-2xl uppercase"> */}
           <Link
             to="/test"
             className="text-center rounded-lg shadow-lg tracking-widest bg-blue-800 text-white text-2xl uppercase py-3 w-full md:1/2 lg:w-1/3 pulse-blue"
           >
             Take Test
           </Link>
-          {/* </button> */}
         </div>
       </div>
     </>
