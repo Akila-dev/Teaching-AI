3b0e42a690c8c68238498ab9890fc9200749eeb8
unknown <akilapanunayan@gmail.com>
2022-07-30 06:04:07 +0100

    first commit

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         id="home"
         className="h-screen w-full container fixed bottom-0 left-0"
       >
-        <div className="flex w-full h-full justify-center lg:items-center items-end pb-14 lg:pb-0">
+        <div className="flex w-full h-full justify-center lg:items-center items-end pb-8 lg:pb-0">
           <Link
             to="/test"
             className="text-center rounded-lg shadow-lg tracking-widest bg-blue-800 text-white text-2xl uppercase py-3 w-full md:1/2 lg:w-1/3 pulse-blue"
